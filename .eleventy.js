a0297b244f1aee06e74417dc3396562372f4117a
WiseGuru <42100212+WiseGuru@users.noreply.github.com>
2023-11-14 16:15:24 -0800

    Update .eleventy.js with Headers

--- .eleventy.js
+++ .eleventy.js
@@ -506,6 +506,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/site/img");
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
+  eleventyConfig.addPassthroughCopy("_headers");
   eleventyConfig.addPlugin(faviconsPlugin, { outputDir: "dist" });
   eleventyConfig.addPlugin(tocPlugin, {
     ul: true,
